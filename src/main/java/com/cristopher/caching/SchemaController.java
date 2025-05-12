5cd353efa1c08085d15e237f1002b44efbdbc0e2
Cristopher <coronelcristopher553@gmail.com>
2025-05-12 18:34:19 -0500

    first commit

---
+++ src/main/java/com/cristopher/caching/SchemaController.java
@@ -0,0 +1,20 @@
+package com.cristopher.caching;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SchemaController {
+
+    private final SchemaService schemaService;
+
+    public SchemaController(SchemaService schemaService) {
+        this.schemaService = schemaService;
+    }
+
+    @GetMapping("/schema")
+    public JsonSchema getSchema(@RequestParam String keyJson) {
+        return schemaService.getSchema(keyJson);
+    }
+}
